7fda7e513aeae144875a40090fa044f24813d72b
andrey <cattalin.htc@gmail.com>
2024-09-22 23:15:49 +0200

    --wip-- [skip ci]

--- src/app/private/dashboard/orders/all-orders/all-orders.component.ts
+++ src/app/private/dashboard/orders/all-orders/all-orders.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { OrdersTableComponent } from "../../../../shared/components/orders-table/orders-table.component";
 import { Order, OrderResponse } from '../../../../shared/models/order';
 import { OrdersService } from '../../../../shared/services/orders.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-all-orders',
   standalone: true,
-  imports: [OrdersTableComponent],
+  imports: [OrdersTableComponent, AsyncPipe],
   templateUrl: './all-orders.component.html',
 })
 export class AllOrdersComponent implements OnInit {
