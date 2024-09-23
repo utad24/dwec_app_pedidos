a168def60babc6d85defdd4a51ea7f1a9156f876
andrey <cattalin.htc@gmail.com>
2024-09-23 14:30:49 +0200

    --wip-- [skip ci]

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -6,12 +6,9 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  console.log('Auth Guard: Checking authentication status...');
   if (authService.isAuthenticated()) {
-    console.log('Auth Guard: User is authenticated');
     return true;
   } else {
-    console.log('Auth Guard: User is not authenticated, redirecting to home');
     router.navigate(['/']);
     return false;
   }
